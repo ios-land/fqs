739bb7b82a9eede9d00bb240351a11e0605dde38
andsto <andrei.stoleru@gmail.com>
2018-12-04 08:56:14 +0100

    Updated server

--- server.go
+++ server.go
@@ -13,6 +13,10 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/ping", pingHandler)
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./index.html")
+	})
+
 	if err := http.ListenAndServe(":3030", nil); err != nil {
 		log.Fatal(err)
 	}
